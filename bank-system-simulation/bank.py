f1860fb427dfc0a726f3a60d7e42f1c3656c0ae5
Bekichemeda <bekichemeda@gmail.com>
2026-01-06 17:57:53 +0300

    Bank system simulator

---
+++ bank-system-simulation/bank.py
@@ -0,0 +1,41 @@
+#pylint:disable=E1101
+#pylint:disable=E0601
+import os
+import json
+import random
+
+class Bank:
+    def __init__(self,first,last,age):
+        self.first = first
+        self.last = last
+        self.age = age
+        self.account_length = 10
+        self.initiate_file()
+        print(self.generate_account())
+    def initiate_file(self):
+        if not os.path.exists(os.path.join(os.path.dirname(__file__), 'accounts.json')):
+            with open(os.path.join(os.path.dirname(__file__), 'accounts.json'), "w") as f:
+                json.dump([],f,indent=4)
+                
+    def load_accounts(self):
+                with open(os.path.join(os.path.dirname(__file__), "accounts.json"), "r") as f:
+                    accounts = json.load(f)
+                return accounts
+                     
+         
+    def is_unique(self,account):
+        accounts = self.load_accounts()
+        if accounts:
+            for a in accounts:
+                if a['account_number'] == account:
+                    return False
+        return True
+    def last_account
+    def generate_account(self):
+        account_number=[]
+        for a in range(self.account_length):
+            account_number.append(str(random.randint(0,9)))
+        account_number = "".join(account_number)
+        return account_number
+            
+bank = Bank("Beki","Chemeda",18)
